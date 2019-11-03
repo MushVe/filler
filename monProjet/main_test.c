407be7b8229f7c4f832264117f3fbce0789047c2
MushVe <c.seguiercuesta@free.fr>
2019-11-03 03:30:12 +0100

    not_blinking_anymore

--- monProjet/main_test.c
+++ monProjet/main_test.c
@@ -48,43 +48,12 @@ int main()
 	int window_height = (grid_height * grid_cell_size) + 1;
 	
 	int i = 0;
-	// tab[i].x = 14;
-	// tab[i].y = 12;
-	// tab[i].player = 'X';
-	// ++i;
-	// tab[i].x = 2;
-	// tab[i].y = 8;
-	// tab[i].player = 'O';
-	// while (i++ < 17 * 15)
-	// {
-	// 	tab[i].x = -1;
-	// 	tab[i].y = -1;
-	// 	tab[i].player = -1;
-	// }
+
 	hardcode_players(tab);
 	init_grid(tab);
 
-
-
-	// // create X square
-	// SDL_Rect x_square = {
-	// 	.x = 14 * grid_cell_size,
-	// 	.y = 12 * grid_cell_size,
-	// 	.w = grid_cell_size,
-	// 	.h = grid_cell_size,
-	// };
-
-	// // create O square
-	// SDL_Rect o_square = {
-	// 	.x = 2 * grid_cell_size,
-	// 	.y = 8 * grid_cell_size,
-	// 	.w = grid_cell_size,
-	// 	.h = grid_cell_size,
-	// };
-
-	// Dark theme.
-	SDL_Color background = {22, 22, 22, 255}; // Barely Black
-	SDL_Color grid_line_color = {44, 44, 44, 255}; // Dark grey
+	SDL_Color background = {20, 20, 20, 255}; 
+	SDL_Color grid_line_color = {70, 70, 70, 255};
 	SDL_Color x_square_color = {22, 200, 200, 255};
 	SDL_Color x_old_square_color = {22, 100, 100, 255};
 	SDL_Color o_square_color = {200, 200, 22, 255};
@@ -110,10 +79,7 @@ int main()
 
 	SDL_bool quit = SDL_FALSE;
 
-	// Draw background.
-	SDL_SetRenderDrawColor(renderer, background.r, background.g,
-		background.b, background.a);
-	SDL_RenderClear(renderer);
+	
 
 	SDL_Rect x_square;
 	SDL_Rect o_square;
@@ -123,6 +89,11 @@ int main()
 	while (!quit) 
 	{
 		SDL_Event event;
+		// Draw background.
+		SDL_SetRenderDrawColor(renderer, background.r, background.g,
+			background.b, background.a);
+		SDL_RenderClear(renderer);
+		
 		while (SDL_PollEvent(&event)) 
 		{
 			switch (event.type) 
@@ -138,68 +109,54 @@ int main()
 			switch (tab[i].player)
 			{
 				case 'x':
-					// create X square
+					// set X square
 					set_square(&x_square, tab[i], grid_cell_size);
-					// x_square.x = tab[i].x * grid_cell_size;
-					// x_square.y = tab[i].y * grid_cell_size;
-					// x_square.w = grid_cell_size;
-					// x_square.h = grid_cell_size;
 					// draw x square
 					SDL_SetRenderDrawColor(renderer, x_square_color.r,
 						x_square_color.g, x_square_color.b, x_square_color.a);
 					SDL_RenderFillRect(renderer, &x_square);
 					break;
 				case 'o':
-					// create O square
-					// o_square.x = tab[i].x * grid_cell_size;
-					// o_square.y = tab[i].y * grid_cell_size;
-					// o_square.w = grid_cell_size;
-					// o_square.h = grid_cell_size;
-					// draw o square
+					// set O square
 					set_square(&o_square, tab[i], grid_cell_size);
+					// draw o square
 					SDL_SetRenderDrawColor(renderer, o_square_color.r,
 						o_square_color.g, o_square_color.b, o_square_color.a);
 					SDL_RenderFillRect(renderer, &o_square);
 					break;
 				case 'X':
-					// create X_old square
-					// x_old_square.x = tab[i].x * grid_cell_size;
-					// x_old_square.y = tab[i].y * grid_cell_size;
-					// x_old_square.w = grid_cell_size;
-					// x_old_square.h = grid_cell_size;
-					// draw x square
+					// set X_old square
 					set_square(&x_old_square, tab[i], grid_cell_size);
+					// draw x square
 					SDL_SetRenderDrawColor(renderer, x_old_square_color.r,
 						x_old_square_color.g, x_old_square_color.b, x_old_square_color.a);
 					SDL_RenderFillRect(renderer, &x_old_square);
 					break;
 				case 'O':
-					// create O_old square
-					// o_old_square.x = tab[i].x * grid_cell_size;
-					// o_old_square.y = tab[i].y * grid_cell_size;
-					// o_old_square.w = grid_cell_size;
-					// o_old_square.h = grid_cell_size;
-					// draw o square
+					// set O_old square
 					set_square(&o_old_square, tab[i], grid_cell_size);
+					// draw o square
 					SDL_SetRenderDrawColor(renderer, o_old_square_color.r,
 						o_old_square_color.g, o_old_square_color.b, o_old_square_color.a);
 					SDL_RenderFillRect(renderer, &o_old_square);
 					break;
 				default: break;
+			}
 		}
-	}
 
 		// Draw grid lines.
 		SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
 				grid_line_color.b, grid_line_color.a);
 
 		for (int x = 0; x < 1 + grid_width * grid_cell_size;
-				x += grid_cell_size) {
+				x += grid_cell_size) 
+		{
 			SDL_RenderDrawLine(renderer, x, 0, x, window_height);
 		}
 
 		for (int y = 0; y < 1 + grid_height * grid_cell_size;
-				y += grid_cell_size) {
+				y += grid_cell_size)
+		{
 			SDL_RenderDrawLine(renderer, 0, y, window_width, y);
 		}
 		// display all
@@ -214,37 +171,3 @@ int main()
 
 	return EXIT_SUCCESS;
 }
-
-		// // Draw grid background.
-		// SDL_SetRenderDrawColor(renderer, grid_background.r, grid_background.g,
-		// 		grid_background.b, grid_background.a);
-		// // SDL_RenderClear(renderer);
-
-		// // draw old x square
-		// SDL_SetRenderDrawColor(renderer, x_old_square_color.r, x_old_square_color.g,
-		// 		x_old_square_color.b, x_old_square_color.a);
-		// SDL_RenderFillRect(renderer, &x_square);
-
-		// // draw old o square
-		// SDL_SetRenderDrawColor(renderer, o_old_square_color.r, o_old_square_color.g,
-		// 		o_old_square_color.b, o_old_square_color.a);
-		// SDL_RenderFillRect(renderer, &o_square);
-
-		// // Draw grid lines.
-		// SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
-		// 		grid_line_color.b, grid_line_color.a);
-
-		// for (int x = 0; x < 1 + grid_width * grid_cell_size;
-		// 		x += grid_cell_size) {
-		// 	SDL_RenderDrawLine(renderer, x, 0, x, window_height);
-		// }
-
-		// for (int y = 0; y < 1 + grid_height * grid_cell_size;
-		// 		y += grid_cell_size) {
-		// 	SDL_RenderDrawLine(renderer, 0, y, window_width, y);
-		// }
-
-		// SDL_RenderPresent(renderer);
-		// 		SDL_Delay(1000);
-		
-		//}
