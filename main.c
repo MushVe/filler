d1e0d9e3aa1a3f9cdcb274105cfcca44fa3d3d4f
MushVe <c.seguiercuesta@free.fr>
2019-11-07 06:35:05 +0100

    suppress_hardcode

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cseguier <cseguier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 17:22:41 by cseguier          #+#    #+#             */
-/*   Updated: 2019/11/07 06:08:45 by cseguier         ###   ########.fr       */
+/*   Updated: 2019/11/07 06:34:50 by cseguier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,154 +24,6 @@ void init_tab(t_coord *tab, t_p *p)
 		tab[i].y = -1;
 		tab[i].player = -1;
 	}
-}
-
-
-void	hard1(t_coord *tab)
-{
-	int i = 0;
-	tab[i].x = 5;
-	tab[i].y = 10;
-	tab[i].player = 'O';
-	i++;
-	tab[i].x = 7;
-	tab[i].y = 4;
-	tab[i].player = 'X';
-	i++;
-	tab[i].x = -1;
-	tab[i].y = -1;
-	tab[i].player = -1;
-}
-
-void	hard2(t_coord *tab)
-{
-	int i = 0;
-	tab[i].x = 5;
-	tab[i].y = 10;
-	tab[i].player = 'o';
-	i++;
-	tab[i].x = 7;
-	tab[i].y = 4;
-	tab[i].player = 'X';
-	i++;
-	tab[i].x = 6;
-	tab[i].y = 10;
-	tab[i].player = 'o';
-	i++;
-	tab[i].x = 6;
-	tab[i].y = 11;
-	tab[i].player = 'o';
-	i++;
-	tab[i].x = -1;
-	tab[i].y = -1;
-	tab[i].player = -1;
-}
-
-void	hard3(t_coord *tab)
-{
-	int i = 0;
-	tab[i].x = 5;
-	tab[i].y = 10;
-	tab[i].player = 'O';
-	i++;
-	tab[i].x = 7;
-	tab[i].y = 4;
-	tab[i].player = 'x';
-	i++;
-	tab[i].x = 6;
-	tab[i].y = 10;
-	tab[i].player = 'O';
-	i++;
-	tab[i].x = 6;
-	tab[i].y = 11;
-	tab[i].player = 'O';
-	i++;
-	tab[i].x = 8;
-	tab[i].y = 4;
-	tab[i].player = 'x';
-	i++;
-	tab[i].x = 9;
-	tab[i].y = 4;
-	tab[i].player = 'x';
-	i++;
-	tab[i].x = -1;
-	tab[i].y = -1;
-	tab[i].player = -1;
-
-}
-
-void	hard4(t_coord *tab)
-{
-	int i = 0;
-	tab[i].x = 5;
-	tab[i].y = 10;
-	tab[i].player = 'O';
-	i++;
-	tab[i].x = 7;
-	tab[i].y = 4;
-	tab[i].player = 'X';
-	i++;
-	tab[i].x = 6;
-	tab[i].y = 10;
-	tab[i].player = 'o';
-	i++;
-	tab[i].x = 6;
-	tab[i].y = 11;
-	tab[i].player = 'o';
-	i++;
-	tab[i].x = 8;
-	tab[i].y = 4;
-	tab[i].player = 'X';
-	i++;
-	tab[i].x = 9;
-	tab[i].y = 4;
-	tab[i].player = 'X';
-	i++;
-	tab[i].x = 6;
-	tab[i].y = 12;
-	tab[i].player = 'o';
-	i++;
-	tab[i].x = 7;
-	tab[i].y = 12;
-	tab[i].player = 'o';
-	i++;
-	tab[i].x = -1;
-	tab[i].y = -1;
-	tab[i].player = -1;
-}
-
-int hardcode(t_p *p)
-{
-	t_coord	*tab;
-
-	tab = NULL;
-
-	if (!(tab = (t_coord*)ft_memalloc(sizeof(t_coord) * p->board_size)))
-		return (0);
-	//hardcode for test display
-
-	ft_printf("Before INIT\n");
-	init_tab(tab, p);
-	printlist(p);
-	ft_printf("Before fill tab\n");
-	hard1(tab);
-	printlist(p);
-	ft_printf("Before new node\n");
-	new_node(tab, p);
-	ft_printf("First hardcode row ok\n");
-
-	init_tab(tab, p);
-	hard2(tab);
-	new_node(tab, p);
-	init_tab(tab, p);
-	hard3(tab);
-	new_node(tab, p);
-	init_tab(tab, p);
-	hard4(tab);
-	new_node(tab, p);
-	ft_printf("Hardcode end ok\n");
-
-	return (0);
 }
 
 void parser(t_p *p)
@@ -208,17 +60,14 @@ void parser(t_p *p)
 int	main()
 {
 	t_p p;
+
 	//init struct ect
 	p.first = NULL;
 	p.board_size = 0;
-
 	ft_printf("Very start\n");
-	parser(&p); //create grid
-//	p.board_size = 15 * 17;
-	ft_printf("Grid created\n");
+	parser(&p); //create and fill grid
+	ft_printf("Grid created and filled\n");
 	ft_printf("%d\n", p.board_size);
-	hardcode(&p); //fill grid
-	ft_printf("Content Hardcoded\n");
 	printlist(&p);
 	display(&p); //print 
 	ft_printf("End and OK\n");
