647e9fe0e06a6613e0efd80c27c7160c1691be96
MushVe <c.seguiercuesta@free.fr>
2020-01-28 01:40:26 +0100

    makefileok

--- display/display.c
+++ display/display.c
@@ -1,52 +1,106 @@
-#include "display/include/SDL.h"
-#include <stdbool.h>
-#include "libft/includes/ft_printf.h"
-#include "libft/includes/libft.h"
-#include <fcntl.h>
-#include "filler.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cseguier <cseguier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/28 01:22:49 by cseguier          #+#    #+#             */
+/*   Updated: 2020/01/28 01:27:31 by cseguier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void set_square(SDL_Rect *square, t_coord tab, int grid_cell_size)
+#include "include/SDL.h"
+#include "../filler.h"
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+// int		get_board(char *line, t_coord *tab, t_p *p)
+// {
+// 	int	i;
+
+// 	i = -1;
+// 	while (line[++i] != '\0')
+// 	{
+// 		if (!(ft_isdigit(line[i])) && line[i] != ' ' && line[i] != '.')
+// 		{
+// 			tab[p->cpt].player = line[i];
+// 			tab[p->cpt].x = (i - 4);
+// 			tab[p->cpt].y = ft_atoi(line);
+// 			p->cpt++;
+// 		}
+// 	}
+// 	return (0);
+// }
+
+size_t		align(size_t size)
 {
-	square->x = tab.x * grid_cell_size;
-	square->y = tab.y * grid_cell_size;
+	return ((size + 4096) & ~4096);
+}
+
+void set_square(SDL_Rect *square, int x, int y, int grid_cell_size)
+{
+	square->x = x * grid_cell_size;
+	square->y = y * grid_cell_size;
 	square->w = grid_cell_size;
 	square->h = grid_cell_size;
 }
 
-int display(t_p *p)
+char *detect_uwu(char *seek_u)
+{	
+	return strchr(seek_u, 'u');
+}
+
+int main()
 {
+	int fd;
+	struct stat stat;
+	int	player;
+	int x = 0;
+	int	y = 0;
+	int	last = 0;
+	int	stop = 0;
+
+	fd = open("../data", O_RDONLY);
+	fstat(fd, &stat);
+
+	char *to_parse = mmap(NULL, align(stat.st_size), PROT_READ, MAP_PRIVATE, fd, 0);
+//	printf("{%s}\n", to_parse);
 	int grid_cell_size = 20;
-	int grid_width = 17;
-	int grid_height = 15;
-
-	int window_width = (grid_width * grid_cell_size) + 1;
-	int window_height = (grid_height * grid_cell_size) + 1;
+	int grid_lenght;
+	int grid_height;
+	int	i = -1;
 	
-	int i;
+	while (to_parse[++i] != '\n')
+	{
+	//	printf("{%c}\n", to_parse[i]);
+		if (to_parse[i] == 'H')
+			grid_height = atoi(to_parse + i + 1);
+		if (to_parse[i] == 'L')
+			grid_lenght = atoi(to_parse + i + 1);
+	}
 
-	t_lst_coord	*cpy;
-	cpy = p->first;
+	int window_lenght = (grid_lenght * grid_cell_size) + 1;
+	int window_height = (grid_height * grid_cell_size) + 1;
 
 	SDL_Color background = {20, 20, 20, 255}; 
 	SDL_Color grid_line_color = {70, 70, 70, 255};
 	SDL_Color x_square_color = {22, 200, 200, 255};
-	SDL_Color x_old_square_color = {22, 100, 100, 255};
 	SDL_Color o_square_color = {200, 200, 22, 255};
-	SDL_Color o_old_square_color = {100, 100, 22, 255};
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Initialize SDL: %s", SDL_GetError());
-		return EXIT_FAILURE;
-	}
+		return EXIT_FAILURE;}
 
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
-	if (SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window, &renderer) < 0)
+	if (SDL_CreateWindowAndRenderer(window_lenght, window_height, 0, &window, &renderer) < 0)
 	{
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Create window & renderer: %s", SDL_GetError());
-		return EXIT_FAILURE;
-	}
+		return EXIT_FAILURE;}
 
 	SDL_SetWindowTitle(window, "SDL Grid");
 
@@ -54,10 +108,8 @@ int display(t_p *p)
 
 	SDL_Rect x_square;
 	SDL_Rect o_square;
-	SDL_Rect x_old_square;
-	SDL_Rect o_old_square;
 
-	while (!quit) 
+	while (!quit)
 	{
 		SDL_Event event;
 		SDL_SetRenderDrawColor(renderer, background.r, background.g, background.b, background.a);
@@ -66,47 +118,57 @@ int display(t_p *p)
 		{
 			switch (event.type) 
 			{
-			case SDL_QUIT:
-				quit = SDL_TRUE;
-				break;
+				case SDL_QUIT:
+					quit = SDL_TRUE;
+					break;
 			}
-		}
-		i = -1;
-		while (cpy->tab[++i].player != -1)
-		{
-			switch (cpy->tab[i].player)
+		}		
+		// while (to_parse[i] != '\0')
+		// {
+			if (to_parse[i] == '\0')
 			{
-				case 'x':
-					set_square(&x_square, cpy->tab[i], grid_cell_size);
-					SDL_SetRenderDrawColor(renderer, x_square_color.r, x_square_color.g, x_square_color.b, x_square_color.a);
-					SDL_RenderFillRect(renderer, &x_square);
-					break;
-				case 'o':
-					set_square(&o_square, cpy->tab[i], grid_cell_size);
-					SDL_SetRenderDrawColor(renderer, o_square_color.r, o_square_color.g, o_square_color.b, o_square_color.a);
-					SDL_RenderFillRect(renderer, &o_square);
-					break;
-				case 'X':
-					set_square(&x_old_square, cpy->tab[i], grid_cell_size);
-					SDL_SetRenderDrawColor(renderer, x_old_square_color.r, x_old_square_color.g, x_old_square_color.b, x_old_square_color.a);
-					SDL_RenderFillRect(renderer, &x_old_square);
-					break;
-				case 'O':
-					set_square(&o_old_square, cpy->tab[i], grid_cell_size);
-					SDL_SetRenderDrawColor(renderer, o_old_square_color.r, o_old_square_color.g, o_old_square_color.b, o_old_square_color.a);
-					SDL_RenderFillRect(renderer, &o_old_square);
-					break;
-				default: break;
+				stop = 1;
 			}
-		}
-		SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g, grid_line_color.b, grid_line_color.a);
-		for (int x = 0; x < 1 + grid_width * grid_cell_size; x += grid_cell_size) 
-			SDL_RenderDrawLine(renderer, x, 0, x, window_height);
-		for (int y = 0; y < 1 + grid_height * grid_cell_size; y += grid_cell_size)
-			SDL_RenderDrawLine(renderer, 0, y, window_width, y);
-		SDL_RenderPresent(renderer);
-				SDL_Delay(1000);
-		cpy = cpy->next;
+			while (!stop && to_parse[++i] != 'u' && to_parse[i] != '\0')
+			{
+				//printf("{c: %c}\n", to_parse[i]);
+				if (to_parse[i] == 'x')
+					x = atoi(to_parse + i + 1);
+				if (to_parse[i] == 'y')
+					y = atoi(to_parse + i + 1);
+				if (to_parse[i] == 'p')
+					player = atoi(to_parse + i + 1);
+				if (to_parse[i] == 'l')
+					last = atoi(to_parse + i + 1);
+				if (to_parse[i] == '\n')
+				{
+					switch (player)
+					{
+						case 'X':
+							set_square(&x_square, x, y, grid_cell_size);
+							SDL_SetRenderDrawColor(renderer, x_square_color.r, x_square_color.g, x_square_color.b, x_square_color.a);
+							SDL_RenderFillRect(renderer, &x_square);
+							break;
+						case 'O':
+							set_square(&o_square, x, y, grid_cell_size);
+							SDL_SetRenderDrawColor(renderer, o_square_color.r, o_square_color.g, o_square_color.b, o_square_color.a);
+							SDL_RenderFillRect(renderer, &o_square);
+							break;
+						default: break;
+					}
+				}
+			}
+			if (to_parse[i] == 'u')
+			{
+				SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g, grid_line_color.b, grid_line_color.a);
+				for (int x = 0; x < 1 + grid_lenght * grid_cell_size; x += grid_cell_size) 
+					SDL_RenderDrawLine(renderer, x, 0, x, window_height);
+				for (int y = 0; y < 1 + grid_height * grid_cell_size; y += grid_cell_size)
+					SDL_RenderDrawLine(renderer, 0, y, window_lenght, y);
+				SDL_RenderPresent(renderer);
+					SDL_Delay(300);
+			}
+		//}
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
